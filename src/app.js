2d6331cafbe678f737b3f2c18063f7acb8e694d0
TomSawyer1 <117282498+TomSawyer1@users.noreply.github.com>
2025-02-24 23:56:19 +0100

    app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(morgan("dev"));
+app.use(morgan("dev")); //Cursor
 
 
 app.use("/api/auth", authRoutes);
